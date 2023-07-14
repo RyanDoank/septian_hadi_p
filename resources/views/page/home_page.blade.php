0058305639e66a7997cd74f9944a483bc27ec99e
DESKTOP-06UU8V2\septian <septianhadip2002@gmail.com>
2023-07-15 02:03:49 +0700

    home screen

--- resources/views/page/home_page.blade.php
+++ resources/views/page/home_page.blade.php
@@ -1,15 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
-    {{-- <style>body{background-color: aqua}</style> --}}
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="style.css">
-    <title>Document</title>
-</head>
+
+    {{-- CSS --}}
+    <link rel="stylesheet"  href="{{asset('css/home_page.css')}}" type="text/css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    {{-- <link rel="stylesheet" href="app.css"> --}}
+    {{-- <link rel="stylesheet" href="homes.css " type="text/css"/> --}}
+
+    <title>Septian Hadi P</title>
+</head> 
 <body>
-    <h1>hello</h1>
+    <header>
+        <a href="#", class="logo">Septian Hadi P</a>
+        <ul>
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Work</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </header>
+    <section>
+        <img src="asset/profile/blue-pattern.jpg" class="bag_img">
+        <div class="profile">
+            <div class="name_profile">
+                <h2>Septian Hadi P</h2>
+                <h3>Web Developer</h3>
+                <p>
+                    Saya Akrab dipanggil, Tian, Saya lulusan Jurusan Multimedia Dari SMK Bakti Mandiri Bekasi. <br>
+                    Saya adalah seorang yang memiliki ketertarikan dengan technologi. Saat ini saya lebih banyak belajar dalam dunia teknologi informasi dan mencari pengalaman di bidang tersebut.
+                </p>
+                <ul>
+                    <li style="--clr:#dd2a7b"><i class="fa fa-instagram" aria-hidden="true"></i></li>
+                    <li style="--clr:#25d366"><i class="fa fa-whatsapp" aria-hidden="true"></i></li>
+                    <li style="--clr:#0000ff"><i class="fa fa-facebook" aria-hidden="true"></i></li>
+                    <li style="--clr:#1da1f2"><i class="fa fa-twitter" aria-hidden="true"></i></li>
+                </ul>
+            </div>
+            <div class="img_profile">
+                <img src="asset/profile/me2.jpg" class="img_me2">
+            </div>
+        </div>
+    </section>
+    <div class="sec">
+        <h2>Hello</h2>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Quaerat quidem quibusdam quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaerat quaer at quaerat quaerat qu Authors qu Authors qu Authors qu Authors qu Authors qu Authors qu Authors qu Authors qu <br>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus tenetur non amet omnis! Perferendis eos explicabo nisi unde perspiciatis natus saepe repellendus quo? Eaque dolore ducimus natus a, harum atque? Etiam null tempor null Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos rem, obcaecati eveniet asperiores dolorem itaque in cumque omnis earum mollitia, molestias dolores ipsa id hic veritatis nostrum ad odit modi. Cum soc tempor incidid id est laborum et accus ad minim ven ================================================================= <br>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid repellendus animi quod, magnam veniam autem mollitia ducimus minus enim fugit. Enim ad suscipit tempore similique aperiam sunt facilis dignissimos molestiae?<br><br>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet dicta ea quos consequatur quo, accusantium officiis nihil error quidem nisi corporis commodi mollitia incidunt voluptates non dolor consectetur illo placeat.
+        </p>
+    </div>
+    {{-- java script --}}
+    {{-- <script src = "main.js"></script> --}}
 </body>
 </html>
